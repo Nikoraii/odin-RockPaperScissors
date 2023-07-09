a00c1ab37f876e7bc8bc00a64738f81db8fb2d32
Nikoraii <kolja.st@gmail.com>
2023-07-09 16:49:33 +0200

    Add game function, play untill score of 5 is reached

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const ROUNDS = 5;
 
 let playerWins = 0;
 let computerWins = 0;
+let winner = "";
 
 const choice = ["Rock", "Paper", "Scissors"];
 
@@ -25,14 +26,45 @@ function playRound(computerChoice, playerChoice) {
     playerChoice = playerChoice.toLowerCase();
 
     if (computerChoice == playerChoice) {
-        return "It's a tie!"
+        return "tie"
     } else if (
         (computerChoice == "rock" && playerChoice == "scissors") ||
         (computerChoice == "paper" && playerChoice == "rock") ||
         (computerChoice == "scissors" && playerChoice == "paper")
     ) {
-        return "You lose. Try again!";
+        return "lose";
     } else {
-        return "You win!";
+        return "win";
     }
 }
+
+/**
+ * Play rounds until winner is decided
+ * Log Winner
+ */
+function game() {
+    do {
+        let roundWinnder = playRound(getComputerChoice(), "rock");
+        if (roundWinnder == "tie") {
+            console.log("It's a tie!")
+        } else if (roundWinnder == "win") {
+            console.log("You win!");
+            playerWins++;
+        } else {
+            console.log("You lose. Try again!");
+            computerWins++;
+        }
+        console.log(`Computer: ${computerWins} | Player: ${playerWins}`);
+        if (computerWins == 5) {
+            winner = "computer";
+        } else if (playerWins == 5) {
+            winner = "player";
+        }
+
+    } while (winner == "")
+
+    if (winner == "player") { console.log("PLAYER IS WINNER!") }
+    else { console.log("COMPUTER IS WINNER") }
+}
+
+game();
